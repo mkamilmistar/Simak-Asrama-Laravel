0e46177e5af2361ba8df752fbab79c039ac40b2f
mkamilmistar <kamil_mistar@apps.ipb.ac.id>
2020-03-13 18:35:08 +0700

    bareng satria

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,19 +9,58 @@
 
     <link rel="apple-touch-icon" href="{{asset('sites/images/favicon.png')}}">
     <link rel="shortcut icon" href="{{asset('sites/images/favicon.png')}}">
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/navbar-static/">
+
+<!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js.map">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="navbar-top.css" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('sites/assets/css/home.css')}}">
 </head>
 <body>
-    <div id="top-panel" class="top-panel">
-        <header class="header" id="header">
-            <div class="top-left">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="./"><img src="{{asset('sites/images/logo.png')}}" alt="Logo">SIMAK ASRAMA SCB</a>
-                    <a class="navbar-brand hidden" href="./"><img src="{{asset('sites/images/logo2.png')}}" alt="Logo"></a>
-                    <a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a>
-                </div>
+    <header>
+        <nav class="navbar navbar-expand-md bg-green">
+            <a class="navbar-brand geser" href="./"><img src="{{asset('sites/images/logo.png')}}" alt="Logo"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav geserkanan">
+                    <li class="nav-item">
+                        <a class="nav-link" href="google.com"><i class="fa fa-home"></i> Beranda</a>
+                    </li> 
+                    <li class="nav-item">
+                        <a class="nav-link" href="fb.com"><i class="fa fa-info"></i> Tentang SCB</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="fb.com"><i class="fa fa-question-circle"></i> FAQ</a>
+                    </li>
+                    <li class="nav-item"> 
+                        <a class="nav-link" href="fb.com"><i class="fa fa-user"></i> Login</a>
+                    </li>
+                </ul>
+                
             </div>
-        </header>
+        </nav>
+    </header>
+    <div class="content">
+        <div class="kotakgaya">
+            <div class="isi">
+                <h1>Sistem Informasi Asrama</h1>
+                <p>Silahkan Login untuk masuk ke dalam sistem.</p>
+            </div>
+        </div>
+        <div class="kotakputih">
+            
+        </div>
     </div>
+    
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 </body>
 </html>
