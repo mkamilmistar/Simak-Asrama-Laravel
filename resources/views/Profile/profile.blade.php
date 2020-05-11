167231a8fba52abaa3166206c01932809ecabcad
mkamilmistar <kamil_mistar@apps.ipb.ac.id>
2020-05-11 17:30:15 +0700

    a

--- resources/views/Profile/profile.blade.php
+++ resources/views/Profile/profile.blade.php
@@ -44,66 +44,65 @@
                                 <!-- DATA AMALAN -->
                                 <div class="container">
     <!-- Row -->
-    <!-- Section header -->
-    <div class="page-content content">
-        <div class="section-header text-center">
-            <h2 class="title">{{$user->nama}}'s Profile</h2>
-            Profile
-        </div>
-        <div class="contact--info inner ">
-            <div class="row justify-content-center">
-                <img src="{{$user->getAvatar()}}" class="img-circle" style="width:300px;height:300px;object-fit: cover;border-radius:8px" alt="Avatar">
-            </div>
-           
-            <div class="profile-stat">
-                <div class="row justify-content-center">
-                    
-    
-                </div>
-                <div class="row justify-content-center">
-                   
-                    <a href="#" class="main-btn">Tes</a>
-                  
-                </div>
-                <br>
-            </div>
-         
-            <!-- Contact Info -->
-            <div id="main" class="col-md-12">
-                <div class="col-12 col-lg-12">
-                    <h4>Basic Info</h4>
-                    <div class="textprofile">
-                        <a href="/catatan-kebaikan-siswa/{{$user->id}}">Lihat Catatan Kebaikan & Keburukan</a>
-        
-                    </div>
-                    <ul class="contact-list">
-                     
-                        <li>
-                            <h6><i class="fa fa-user" aria-hidden="true"></i>Nama : {{$user->nama}}</h6>                          
-                        </li>
-                        <li>
-                            <h6><i class="fa fa-user" aria-hidden="true"></i>Username : {{$user->username}}</h6>                          
-                        </li>
-                        <li>
-                            <h6><i class="fa fa-user" aria-hidden="true"></i>Nama : {{$user->email}}</h6>                          
-                        </li>
-                        <li>
-                            <h6><i class="fa fa-user" aria-hidden="true"></i>Nama : {{$user->role}}</h6>                          
-                        </li>
-                       
-                        
-                    </ul>
-                    <div class="row justify-content-center">
-                        <a href="/profile/{{$user -> id}}/edit" class="btn btn-warning">Edit</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+                                <!-- Section header -->
+                                <div class="page-content content">
+                                    <div class="section-header text-center">
+                                        <h2 class="title">{{$user->nama}}'s Profile</h2>
+                                        Profile
+                                    </div>
+                                    <div class="contact--info inner ">
+                                        <div class="row justify-content-center">
+                                            <img src="{{$user->getAvatar()}}" class="img-circle" style="width:300px;height:300px;object-fit: cover;border-radius:8px" alt="Avatar">
+                                        </div>
+                                    
+                                        <div class="profile-stat">
+                                            <div class="row justify-content-center">
+                                                
+                                
+                                            </div>
+                                            <div class="row justify-content-center">
+                                            
+                                                <a href="#" class="main-btn">Tes</a>
+                                            
+                                            </div>
+                                            <br>
+                                        </div>
+                                    
+                                        <!-- Contact Info -->
+                                        <div id="main" class="col-md-12">
+                                            <div class="col-12 col-lg-12">
+                                                <h4>Basic Info</h4>
+                                                <div class="textprofile">
+                                                    <a href="/catatan-kebaikan-siswa/{{$user->id}}">Lihat Catatan Kebaikan & Keburukan</a>
+                                    
+                                                </div>
+                                                <ul class="contact-list">
+                                                
+                                                    <li>
+                                                        <h6><i class="fa fa-user" aria-hidden="true"></i>Nama : {{$user->nama}}</h6>                          
+                                                    </li>
+                                                    <li>
+                                                        <h6><i class="fa fa-user" aria-hidden="true"></i>Username : {{$user->username}}</h6>                          
+                                                    </li>
+                                                    <li>
+                                                        <h6><i class="fa fa-user" aria-hidden="true"></i>Nama : {{$user->email}}</h6>                          
+                                                    </li>
+                                                    <li>
+                                                        <h6><i class="fa fa-user" aria-hidden="true"></i>Nama : {{$user->role}}</h6>                          
+                                                    </li>
+                                                
+                                                    
+                                                </ul>
+                                                <div class="row justify-content-center">
+                                                    <a href="/profile/{{$user -> id}}/edit" class="btn btn-warning">Edit</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
 
 
-        <!-- END PROFILE HEADER -->
-    </div>
-</div>
+                                    <!-- END PROFILE HEADER -->
+                                </div>
 </div>
                                 <!-- END OF DATA SISWA -->
                             </div>
