ff95b87d0cb0df9febe5772189e0566157d85260
circleous <circleousdev@gmail.com>
2020-05-20 07:11:38 +0700

    fix CORS

--- routes/api.php
+++ routes/api.php
@@ -13,14 +13,16 @@ use Illuminate\Http\Request;
 |
 */
 
-// Poin Siswa
-Route::get('/poinSiswa/{nis}', 'PoinKebaikanController@show');
-Route::get('/poinSiswa/{nis}/total', 'PoinKebaikanController@total');
+Route::group(['middleware' => ['cors']], function () {
+    // Poin Siswa
+    Route::get('/poinSiswa/{nis}', 'PoinKebaikanController@show');
+    Route::get('/poinSiswa/{nis}/total', 'PoinKebaikanController@total');
 
-// Catatan Sholat
-Route::get('/catatanSholat', 'SholatController@all');
-Route::get('/catatanSholat/{nis}', 'SholatController@show');
-Route::post('/catatanSholat', 'SholatController@store');
+    // Catatan Sholat
+    Route::get('/catatanSholat', 'SholatController@all');
+    Route::get('/catatanSholat/{nis}', 'SholatController@show');
+    Route::post('/catatanSholat', 'SholatController@store');
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
